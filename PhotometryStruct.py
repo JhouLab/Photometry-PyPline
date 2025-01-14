bde85b7ee55c1db1f655d5350d39b26883e93d1a
Nick S. <nicholasschaffer52@gmail.com>
2025-01-14 17:17:36 -0500

    Changed internal structure names, Added function definitions

--- PhotometryStruct.py
+++ PhotometryStruct.py
@@ -3,16 +3,21 @@ import openpyxl
 
 
 class PhotometryData:
-    def __init__(self, type="pulsed", ptDf=None, mpcDf=None, autoFlprofile=0, cutoff=0.009, id_sessionStart=1, id_sessionEnd=2):
-        self.photometryDf = ptDf
-        self.mpcDf = mpcDf
-        self.autoFlProfile = autoFlprofile
+    def __init__(self, type="pulsed", autoFlProfile=0, cutoff=0.009, id_eventsDict = {}, id_sessionStart=1, id_sessionEnd=2):
+        self.autoFlProfile = autoFlProfile
         #threshold value which we remove samples under (these are samples which the laser was not active for)
         self.cutoff = cutoff
+        self.id_events = id_eventsDict
         self.id_sessionStart = id_sessionStart
         self.id_sessionEnd = id_sessionEnd
-        self.cleanedptDf = None
-        self.binnedPtDf = None
+        #photometry data
+        self.pt_raw = None
+        self.pt_cleaned = None
+        self.pt_binned = None
+        self.pt_alignedEvents = {}
+        #behavioral data
+        self.mpc_data = None
+        self.dlc_data = None
         #normaliztion constant, which is functionally the slope from the caluclated linear regression
         self.normConst = 0
         if type.upper() == "PULSED":
@@ -23,21 +28,36 @@ class PhotometryData:
             raise TypeError("Passed recording type is not recognized. Passed", type,
                             "does not match either 'pulsed' or 'continuous'")
 
+    #Helper function which takes a Med-Pc ID integer and returns a pandas dataframe with all of the timestamps for that ID
     def getTimestampTimes(self, timestampID):
-        if self.mpcDf is not None:
-            tmp = self.mpcDf[self.mpcDf.ID == timestampID].secs
+        if self.mpc_data is not None:
+            tmp = self.mpc_data[self.mpc_data.ID == timestampID].secs
             return tmp.values
         else:
             raise UserWarning("Cannot retrieve timestamps from empty Med-pc dataframe. Does the original data include Med-Pc Data?")
 
-    #assumes clean() has already been ran on data
+    #work in progress
+    def alignEvents(self):
+        if self.mpc_data is None:
+            raise UserWarning("Cannot align events to non-existent Med-Pc Data")
+        else:
+            trials = []
+            try:
+                id_trialStart = self.id_events["id_trialStart"]
+                trials = self.getTimestampTimes(id_trialStart)
+            except:
+                raise TypeError("No Med-pc events for id_trialStart")
+
+
+    #uses cleaned data from pulsed recordings to create bins of each recording window
+    #for each recording window, takes the mean of the signal.
     def binData(self):
         if self.isPulsed is False:
             raise TypeError("Recording type is continuous. Cannot bin data for non-pulsed recordings")
-        if self.cleanedptDf is None:
+        if self.pt_cleaned is None:
             raise UserWarning("This data has not been cleaned. Please run clean() before proceeding.")
         else:
-            idxs = self.cleanedptDf[self.cleanedptDf["StartIdx"] == True].index.tolist()
+            idxs = self.pt_cleaned[self.pt_cleaned["StartIdx"] == True].index.tolist()
             if len(idxs) < 1:
                 raise IndexError("Could not find any samples which would indicate the start of a new recording window")
             rowsList = []
@@ -47,56 +67,59 @@ class PhotometryData:
                 end = None
                 start = None
                 if i == len(idxs):
-                    end = self.photometryDf._465.iloc[-1]
+                    end = self.pt_raw._465.iloc[-1]
                 else:
                     end = idxs[i]
                 start = idxs[i - 1]
 
-                window = self.cleanedptDf.iloc[start:end].mean()
+                window = self.pt_cleaned.iloc[start:end].mean()
                 rowsList.append([window.Time, window._405, window._465, window.norm])
 
-            self.binnedPtDf = pd.DataFrame(rowsList)
-            self.binnedPtDf.columns = ["Time", "_405", "_465", "norm"]
-            self.binnedPtDf.reset_index(drop=True, inplace=True)
-            print(self.binnedPtDf)
+            self.pt_binned = pd.DataFrame(rowsList)
+            self.pt_binned.columns = ["Time", "_405", "_465", "norm"]
+            self.pt_binned.reset_index(drop=True, inplace=True)
+            print(self.pt_binned)
 
+    #cleans raw photometry data (Doric-type only).
     def clean(self):
-        if self.photometryDf is None:
+        if self.pt_raw is None:
             raise UserWarning("No photometry data has been added to this struct. Call readData(fpath) before proceeding")
         else:
-            self.cleanedptDf = self.photometryDf
+            self.pt_cleaned = self.pt_raw
             mapping = {"Time(s)": "Time", "AIn-1 - Dem (AOut-1)": "_405", "AIn-1 - Dem (AOut-2)": "_465",
                        "DI/O-3": "TTL_6", "DI/O-4": "TTL_8"}
-            self.cleanedptDf.rename(columns=mapping, inplace=True)
-            #remove samples which are outside recording windows
-            self.cleanedptDf = self.cleanedptDf.drop(self.cleanedptDf[self.cleanedptDf.TTL_6 < 1].index)
-            #remove samples in which signal is close to 0
-            #these are samples where the TTL turned on the recording, but the laser is not fully on
-            self.cleanedptDf = self.cleanedptDf.drop(self.cleanedptDf[self.cleanedptDf._465 < self.cutoff].index)
+            self.pt_cleaned.rename(columns=mapping, inplace=True)
 
-            #remove samples which are before or after session start and end times
-            start = self.getTimestampTimes(self.id_sessionStart)
-            end = self.getTimestampTimes(self.id_sessionEnd)
-            if len(start) == 1:
-                start = start[0]
-            else:
-                raise TypeError("Found more than one session start timestamps in Med-Pc data. Check your Med-Pc file.")
-
-            if len(end) == 1:
-                end = end[0]
-            else:
-                raise TypeError("Found more than one session end timestamps in Med-Pc data. Check your Med-Pc file.")
-            self.cleanedptDf = self.cleanedptDf.drop(self.cleanedptDf[self.cleanedptDf.Time < start].index)
-            self.cleanedptDf = self.cleanedptDf.drop(self.cleanedptDf[self.cleanedptDf.Time > end].index)
-
-            #reset index to consecutive
-            self.cleanedptDf.reset_index(drop=True, inplace=True)
-
-            # find start and end times based on idxs where the time "jumps", signifying a new recording window
-            # remove 2 samples at start and end to exclude points where laser was partially on/off
             if self.isPulsed is True:
-                self.cleanedptDf["StartIdx"] = self.cleanedptDf["Time"].diff() > 1
-                idxs = self.cleanedptDf[self.cleanedptDf.StartIdx].index
+                #remove samples which are outside recording windows
+                self.pt_cleaned = self.pt_cleaned.drop(self.pt_cleaned[self.pt_cleaned.TTL_6 < 1].index)
+                #remove samples in which signal is close to 0
+                #these are samples where the TTL turned on the recording, but the laser is not fully on
+                self.pt_cleaned = self.pt_cleaned.drop(self.pt_cleaned[self.pt_cleaned._465 < self.cutoff].index)
+
+                #remove samples which are before or after session start and end times (if Med-Pc data as been loaded into data structure)
+                if self.mpc_data is not None:
+                    start = self.getTimestampTimes(self.id_sessionStart)
+                    end = self.getTimestampTimes(self.id_sessionEnd)
+                    if len(start) == 1:
+                        start = start[0]
+                    else:
+                        raise TypeError("Found more or less than one session start timestamps in Med-Pc data. Check your Med-Pc file.")
+
+                    if len(end) == 1:
+                        end = end[0]
+                    else:
+                        raise TypeError("Found more or less than than one session end timestamps in Med-Pc data. Check your Med-Pc file.")
+                    self.pt_cleaned = self.pt_cleaned.drop(self.pt_cleaned[self.pt_cleaned.Time < start].index)
+                    self.pt_cleaned = self.pt_cleaned.drop(self.pt_cleaned[self.pt_cleaned.Time > end].index)
+
+                #reset index to consecutive
+                self.pt_cleaned.reset_index(drop=True, inplace=True)
+
+                # find start and end times based on idxs where the time "jumps", signifying a new recording window
+                # remove 2 samples at start and end to exclude points where laser was partially on/off
+                self.pt_cleaned["StartIdx"] = self.pt_cleaned["Time"].diff() > 1
+                idxs = self.pt_cleaned[self.pt_cleaned.StartIdx].index
                 if len(idxs) < 1:
                     raise TypeError("Could not find any samples which would indicate the start of a new recording window")
                 rowsList = []
@@ -105,7 +128,7 @@ class PhotometryData:
                     end = None
                     start = None
                     if i == len(idxs):
-                        end = self.cleanedptDf._465.iloc[-1]
+                        end = self.pt_cleaned._465.iloc[-1]
                         end -= 2
                     else:
                         end = idxs[i]
@@ -113,26 +136,27 @@ class PhotometryData:
                     start = idxs[i - 1]
                     start += 2
                     #need to flag this sample as new start of window
-                    self.cleanedptDf["StartIdx"][start] = True
+                    self.pt_cleaned["StartIdx"][start] = True
 
-                    window = self.cleanedptDf.iloc[start:end]
+                    window = self.pt_cleaned.iloc[start:end]
                     rowsList.append(window)
 
-                    self.cleanedptDf = pd.concat(rowsList)
-                    self.cleanedptDf.reset_index(drop=True, inplace=True)
+                    self.pt_cleaned = pd.concat(rowsList)
+                    self.pt_cleaned.reset_index(drop=True, inplace=True)
 
             self.cleaned = True
 
+    #Normalizes cleaned photometry data
     def normalize(self):
-        if self.cleanedptDf is None:
+        if self.pt_cleaned is None:
             raise UserWarning("This data has not been cleaned. Please run clean() before proceeding.")
         else:
             #take first and last 20 samples, calculate x-intercept of line passing between the points
-            end = len(self.photometryDf._465)
-            y1 = self.photometryDf._465[0:20].mean()
-            y2 = self.photometryDf._465[end - 20:end].mean()
-            x1 = self.photometryDf._405[0:20].mean()
-            x2 = self.photometryDf._405[end - 20:end].mean()
+            end = len(self.pt_raw._465)
+            y1 = self.pt_raw._465[0:20].mean()
+            y2 = self.pt_raw._465[end - 20:end].mean()
+            x1 = self.pt_raw._405[0:20].mean()
+            x2 = self.pt_raw._405[end - 20:end].mean()
 
             intercept = x2 - (y2 * (x1 - x2)) / (y1 - y2)
             print("Slope of regression: ", intercept)
@@ -143,9 +167,10 @@ class PhotometryData:
             self.normConst = intercept
             intercept += self.autoFlProfile
 
-            self.cleanedptDf["norm"] = self.cleanedptDf._465 / (self.cleanedptDf._405 - intercept)
-            print(self.cleanedptDf)
+            self.pt_cleaned["norm"] = self.pt_cleaned._465 / (self.pt_cleaned._405 - intercept)
+            print(self.pt_cleaned)
 
+    #given a path to a .xlsx file, loads Med-Pc and Photometry data into data structure
     def readData(self, fpath):
         rawData = None
         timestampData = None
@@ -159,7 +184,7 @@ class PhotometryData:
         try:
             timestampData = pd.read_excel(fpath, sheet_name="Med-Pc", header=0)
         except:
-            raise RuntimeError("Could not find Med-Pc data in file. Is there an excel tab labeled 'Med-Pc'?")
+            print("Warning: Could not find Med-Pc data in file. Is there an excel tab labeled 'Med-Pc'?")
 
-        self.photometryDf = rawData
-        self.mpcDf = timestampData
+        self.pt_raw = rawData
+        self.mpc_data = timestampData
